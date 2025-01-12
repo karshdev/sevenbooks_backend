332444ebf9bdc10e2731ac992a1b8f6a1ff163b1
karshdev <sharmaaakarsh332@gmail.com>
2025-01-12 18:15:46 +0530

    Added cors options

--- index.ts
+++ index.ts
@@ -26,7 +26,14 @@ const port = Number(process.env.PORT) ?? 5000;
 
 const app: Express = express();
 
-app.use(cors())
+
+const corsOptions = {
+  origin: ["https://sevenbooks-nine.vercel.app"], 
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
+  credentials: true, 
+};
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
